73976873ded2eb7b1515c82397b94192e11c9985
山本　憲 <em072029@gmail.com>
2018-08-26 14:16:38 +0900

    足し算アプリ

--- calcQuizApp/ViewController.swift
+++ calcQuizApp/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    
+    // 足し算に使う変数を用意します。
+    var a = 0
+    var b = 0
+    var correct = 0
+    // 出題をする関数
+    func question(){
+        // ランダムに問題を作ります
+        a = Int(arc4random() % 100)
+        b = Int(arc4random() % 100)
+        correct = a + b
+        // ラベルに問題を表示します。
+        myLabel.text = "\(a) + \(b) = ?"
+        // テキストフィールドを空にします。
+        myTextField.text = ""
+    }
+    
+    @IBAction func tapButton(_ sender: Any) {
+        // 整数が入力されているか
+        guard let answer = Int(myTextField.text!) else {
+            return
+        }
+        // 答え合わせ
+        var check = "間違い"
+        if answer == correct {
+            check = "正解"
+        }
+        
+        // ダイアログを作る
+        let alert = UIAlertController(title: "足し算クイズ", message: check, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler:{action in
+            // 正解なら次の出題
+            if answer == self.correct {
+                self.question()
+            }
+        })
+        alert.addAction(defaultAction)
+        // ダイアログを表示します。
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 出題をする関数を呼び出します。
+        question()
     }
 
     override func didReceiveMemoryWarning() {
